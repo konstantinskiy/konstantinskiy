6c92046e0a4cef94532f0a6f1cb603dbf36ec805
konstantinskiy <konstantinskiy@gmail.com>
2019-12-05 11:19:14 +0200

    add mebelbor project index page

---
+++ mebelbor/js/scripts.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+
+    // Functions for show/hide adaptive container after click
+
+    function toggleAdaptiveMenu() {
+        $('.s-main__side').toggleClass('s-main__side--open');
+        $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+        $('html').toggleClass('scroll-disable');
+    }
+
+    $('.js-adaptive-slide').on('click', function(e) {
+        e.preventDefault();
+        toggleAdaptiveMenu();
+    });
+
+
+
+    // Hero slider init
+
+    if($('.hero-slider').length > 0) {
+        $('.hero-slider').slick({
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        dots: true
+                    }
+                }
+            ]
+        });
+    }
+
+
+
+    // Products slider init
+
+    if($('.products-slider').length > 0) {
+        $('.products-slider').slick({
+            slidesToShow: 4,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1600,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 1280,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+        });
+
+        $('.js-arrow-left').click(function(e) {
+            e.preventDefault();
+            $(this).parents('.s-items').find('.products-slider').slick('slickPrev');
+        });
+
+        $('.js-arrow-right').click(function(e) {
+            e.preventDefault();
+            $(this).parents('.s-items').find('.products-slider').slick('slickNext');
+        });
+    }
+
+
+
+    // Modal init
+
+    $('[data-fancybox-modal]').fancybox({
+        trapFocus: true,
+        autoFocus: false,
+        touch: false,
+        beforeShow: function() {
+            $('html').addClass('scroll-disable');
+        },
+        afterClose: function() {
+            $('html').removeClass('scroll-disable');
+        }
+    });
+
+
+});
