770874fc83169fed941aeb56ad007871018a5b21
konstantinskiy <konstantinskiy@gmail.com>
2020-02-06 18:41:24 +0200

    mebelbor fixes

--- mebelbor/js/scripts.js
+++ mebelbor/js/scripts.js
@@ -608,6 +608,36 @@ $(document).ready(function() {
             myMap.geoObjects.add(placemark);
         }
     }
-        
+
+
+
+    // Is mobile object
+
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    if(isMobile.any()) {
+        $('.sglproduct-choice-accordion__title').removeClass('sglproduct-choice-accordion__title--active');
+        $('.sglproduct-choice-accordion__content').hide();
+    }
+
 
 });
